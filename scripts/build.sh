a7a78a274147ebfdac1a2b8c7960d9abef0fcea5
Matthew Pombo <datacorruption10@gmail.com>
2026-03-19 12:28:14 -0700

    wf cache test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Build script for local development, CI releases, and operated mirrors. :p
+# Build script for local development, CI releases, and operated mirrors.
 #
 # Local:
 #   ./scripts/build.sh
